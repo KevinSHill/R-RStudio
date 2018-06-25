eecefa1a4775e038536d1e2dbdae38a5b6565c70
KevinSHill <kh.aws.consolelogin@gmail.com>
2018-06-25 08:08:12 -0400

    Parallel Processing in R
    processing parallel for loops in R with the 'foreach' package

---
+++ Parallel Processing/foreach.R
@@ -0,0 +1,14 @@
+library(foreach)
+
+(cores <- parallel::detectCores())
+cl <- parallel::makeCluster(cores-1)
+doParallel::registerDoParallel(cl)
+
+results <-foreach(i=1:n, .combine = rbind, .export=c('exportedDataSet','exportedFucntion'), .packages = c("dplyr")) %dopar% {
+
+    #Code the  loop   
+    return(valuesToReturn)
+}
+
+parallel::stopCluster(cl)
+registerDoSEQ()
